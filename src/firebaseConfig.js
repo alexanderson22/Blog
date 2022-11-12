dfded4467592035b72a1e40a8c4c98289451c5c1
Z Anderson <aander79@lion.lmu.edu>
2022-11-12 15:17:28 -0800

    file edits to firebaseConfig

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,9 +1,6 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth"
 import { getFirestore } from "firebase/firestore"
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -15,7 +12,7 @@ const firebaseConfig = {
   appId: "1:80250307864:web:5638f3819a13d23ef20a6d"
 };
 
-// Initialize Firebase
+
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app)
 export const db = getFirestore(app)
